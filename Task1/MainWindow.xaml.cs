d19628d96b5cf298afa5ed994f41986fd0a792eb
student <endkripeer@gmail.com>
2024-09-17 09:57:32 +0500

    решение 1 задачи

---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(txt1.Text);
+            int b = 4 * a;
+            int c = a * a;
+            txt2.Text = "P= " + b;
+            txt3.Text = "S= " + c;
+        }
+    }
+}
